ab64a89c42b076b05d338a6a5a70612a40e955e5
free <free@localhost.localdomain>
2019-01-13 08:25:49 -0700

    WF DEV

--- web filter/webfilter_dev.py
+++ web filter/webfilter_dev.py
@@ -8,49 +8,56 @@ counter = 0
 build_filter = "dst port 53" #& src port 53"
 ## Define our Custom Action function
 
-class CustomAction:
+class WebFilter:
 	def __init__(self):
 		self.DEVNULL = open(os.devnull, 'wb')
+		self.urldict = {}
+		self.dictload()
 		self.sniffer()
+	
+	def dictload(self):
+		with open('Blacklist', 'r') as BL:
+			while True:
+				urL = BL.readline().strip().lower()
+				if (not urL):
+					break	
+				urlP = urL.split('.')
+				F1 = urlP[0]
+				F2 = urlP[1]
+				urLHex = '{}|{:02d}|{}'.format(F1, len(F2), F2) # len(F1), |{:02d}|
+				self.urldict[urL] = [urLHex, 0]		
+#				print(self.urldict)
+	def packet_hold(self):
+	    
+	    
+	    tc qdisc add dev eth0 root netem delay 100ms			
+
 
 	def sniffer(self):
 	
 		sniff(iface="eth0", filter=build_filter, prn=self.custom_action)
 		
 	def custom_action(self, packet):
-#	global counter
-		Q = 0
-		reQ = packet[0][4].qname.decode()
+		reQ = packet[0][4].qname.decode().lower()
+		
 #		packet[0].show()
 		reQ = reQ[:-1]
-		with open('Blacklist', 'r') as BL:
-			with open('/etc/hosts', 'a') as hF:
-				for dN in BL:
-					if reQ in dN:
-						g1, g2 = dN.split(' ')
-#						hF.write('224.1.0.1 {}'.format(dN))
-#						subprocess.call(['iptables', '-A', 'INPUT', '-i', 'eth0', '-m', 'string', '--hex-string', '|05|4chan|03|com', '--algo', 'bm', '-j', 'DROP'])
-#						try:
-#							subprocess.call(['sudo', 'iptables', '-D', 'INPUT', '-m', 'string', '--hex-string', '"{}"'.format(g2.strip('\n')), '--algo', 'bm', '-j', 'DROP'])
-#						except Exception as E:
-#							print(E)
-						g2b = g2.strip()
-						print(g2b)
-						subprocess.call(['sudo', 'iptables', '-A', 'INPUT', '-m', 'string', '--hex-string', '{}'.format(g2b), '--algo', 'bm', '-j', 'DROP'])
-#						res = subprocess.call(['ping', '-c', '1', str(tarGetIP)], stdout = DEVNULL)  
-						print('Blocking {}. hehehe'.format(reQ))
+		reQ2 = 'www.{}'.format(reQ)
 		
+		if (reQ in self.urldict or reQ2 in self.urldict):
+			urL = self.urldict[reQ][0]
+			if (self.urldict[reQ][1] == 0):
+				self.urldict[reQ][1] += 1
+#				print(self.urldict[reQ][1])
+				print(self.urldict[reQ])
+				subprocess.call(['sudo', 'iptables', '-A', 'INPUT', '-m', 'string', '--hex-string', urL, '--algo', 'bm', '-j', 'DROP'])				
+#				self.urldict[reQ][1] = cnt + 1
+				print('Blocking {}. hehehe'.format(reQ))
+			else:
+				self.urldict[reQ][1] += 1
+				print(self.urldict[reQ][1])
+				print('{} already blocked.'.format(reQ))
+				pass
 		
-			
-		
-#		for shit in packet[0][4]:
-#			fuck = shit.decode()
-#			print(fuck)
- 
-## Setup sniff, filtering for IP traffi
 
-
-#def sniffer():
-#	sniff(iface="eth0", filter=build_filter, count=10000, prn=CustomAction)
-
-CustomAction()
+WebFilter()
