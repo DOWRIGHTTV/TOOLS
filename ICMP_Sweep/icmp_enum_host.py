9a0f02bbe34e396340b1810d3da6d8ef39b17690
DOWRIGHT <dowright@nationofnobodies.com>
2020-04-06 16:01:43 -0700

    general refactor

--- ICMP_Sweep/icmp_enum_host.py
+++ ICMP_Sweep/icmp_enum_host.py
@@ -1,70 +1,97 @@
 #!/usr/bin/python3
 
-import threading
 import os, sys
-import subprocess
+import time
+import threading
+
+import argparse
+from sys import argv
 
 from ipaddress import IPv4Network
 from socket import gethostbyname
+from subprocess import run, CalledProcessError, DEVNULL
+
+parser = argparse.ArgumentParser(description = 'resolves then pings enumerated domain names.')
+parser.add_argument('-v', '--verbose', help='prints output to screen', action='store_true')
+parser.add_argument('-c', '--clear', help='clears shell throughout, including on startup, automatically.', action='store_true')
+
+args = parser.parse_args(argv[1:])
+VERBOSE = args.verbose
+CLEAR = args.clear
 
 
 class EnumHost:
     def __init__(self, file, domain):
-        self.file = file
-        self.domain = domain
-        self.reachable_hosts = []
+        self._file = file
+        self._domain = domain
+        self._reachable_hosts = []
+
+        self._sweep_complete = threading.Event()
 
     def __enter__(self):
-        self.devnull = open(os.devnull, 'w')
-
         return self
 
     def __exit__(self, exc_type, exc_val, traceback):
-        self.devnull.close()
-        
         if (exc_type):
             print(exc_val)
-            
+
         return True
 
     def start(self):
-        self.main()
-        self.complete()
+        threading.Thread(target=self._main).start()
 
-    def icmp_thread(self, domain):
+        self._timer(time.time())
+        self._complete()
+
+    def _icmp_thread(self, domain):
         try:
             ip_address = gethostbyname(domain)
-        except OSError as E:
+        except OSError:
+            if (VERBOSE):
+                print(f'dns failure: {domain}.')
             return
-        
-        print(f'pinging ip: {ip_address} for host: {domain}')
-        result = subprocess.run(f'ping -c 1 {ip_address}', shell=True, stdout=self.devnull, stderr=self.devnull)
-        if (not result.returncode):
-            self.reachable_hosts.append(f'{domain} {ip_address}')
 
-    def main(self):
-        threads = []
-        with open(self.file, 'r') as sd:
+        if (VERBOSE):
+            print(f'pinging ip: {ip_address} for host: {domain}')
+        try:
+            run(f'ping -c 1 -W .51 {ip_address}',
+                shell=True, check=True, stdout=DEVNULL, stderr=DEVNULL)
+        except CalledProcessError:
+            pass
+        else:
+            self._reachable_hosts.append(f'{domain} {ip_address}')
+
+    def _main(self):
+        with open(self._file, 'r') as sd:
             sub_domains = sd.read().split()
-            
+
+        threads = []
         for sub_domain in sub_domains:
-            fqdn = f'{sub_domain}.{self.domain}'
-            thread = threading.Thread(target=self.icmp_thread, args=(fqdn,))
+            fqdn = f'{sub_domain}.{self._domain}'
+            threads.append(threading.Thread(target=self._icmp_thread, args=(fqdn,)))
+
+        for thread in threads:
             thread.start()
-            threads.append(thread)
-            
+
         for thread in threads:
             thread.join()
 
-    def complete(self):
-        if self.reachable_hosts:
-            print('||Following Hosts are UP||')
-            print('-' * 24)
-        else:
-            print('NO HOSTS RESPONDED :(.')
+        self._sweep_complete.set()
 
-        for host in sorted(self.reachable_hosts):
-            print(host)
+    def _complete(self):
+        print(f'completed in {self._time_to_sweep} seconds')
+        print('-' * 26)
+        if not self._reachable_hosts:
+            print('NO HOSTS RESPONDED :(.')
+        else:
+
+            for host in sorted(self._reachable_hosts):
+                print(host)
+
+    def _timer(self, start):
+        self._sweep_complete.wait()
+
+        self._time_to_sweep = round(time.time() - start, 2)
 
 def scan_again():
     while True:
@@ -72,21 +99,32 @@ def scan_again():
         if (user_input in ['y', '']):
             break
         elif (user_input == 'n'):
-            exit('exiting...')
+            print('exiting...')
+            os._exit(0)
         else:
             print('invalid entry. try again.')
 
 def main():
     while True:
         user_input = input('enter domain to enumerate: ')
-        enum_file  = input('enter file to load: ')
+        while True:
+            enum_file  = input('enter file to load: ')
+            if (os.path.isfile(enum_file)): break
+
+            print(f'file {enum_file} does not exist. try again.')
+
+        if (CLEAR):
+            run('clear', shell=True)
         with EnumHost(enum_file, user_input) as enum_host:
             enum_host.start()
 
         scan_again()
 
 if __name__ == '__main__':
+    print('Enumerated host name reachability test| by DOWRIGHT. ^_^')
     try:
+        if (CLEAR):
+            run('clear', shell=True)
         main()
     except KeyboardInterrupt:
         exit('\nexiting due to keyboard interrupt!')
--- ICMP_Sweep/icmp_sweep.py
+++ ICMP_Sweep/icmp_sweep.py
@@ -1,97 +1,93 @@
 #!/usr/bin/python3
 
-import time
-import random
-import threading
 import os, sys
-import subprocess
+import time
+import threading
 
+from subprocess import run, SubprocessError, DEVNULL
 from ipaddress import IPv4Network
-from copy import copy
 
 
 class PingSweep:
     def __init__(self, subnet):
-        self.subnet = subnet
-        self.reachable_hosts = []
-        
-        self.ip_count        = 0
-        self.completed_count = 0
-        self.sweep_complete  = False
-        self.counter_lock    = threading.Lock()
+        self._subnet = subnet
+        self._reachable_hosts = []
+
+        self._ip_count        = 0
+        self._completed_count = 0
+        self._counter_lock    = threading.Lock()
+        self._sweep_complete  = threading.Event()
 
     def __enter__(self):
-        self.devnull = open(os.devnull, 'w')
         try:
-            self.ip_network = list(IPv4Network(self.subnet).hosts())
-        except Exception as E:
-            self.ip_network = None
+            self._ip_network = list(IPv4Network(self._subnet).hosts())
+        except Exception:
+            self._ip_network = None
         else:
-            self.ip_count = len(self.ip_network)
+            self._ip_count = len(self._ip_network)
 
         return self
 
     def __exit__(self, exc_type, exc_val, traceback):
-        self.devnull.close()
-        
         return True
 
     def start(self):
-        if (not self.ip_network):
+        if (self._ip_network is None):
             raise TypeError('invalid network subnet. ex. 192.168.100.0/24')
-            
+
         threading.Thread(target=self._main).start()
-        # blocking until all threads finished and completion boolean is set to true
+        # blocking until all threads finished and completion event is set
         self._timer(time.time())
         self._complete()
 
     def _icmp(self, ip_address):
-        result = subprocess.run(f'ping -c 1 -W 1 {ip_address}', shell=True, stdout=self.devnull, stderr=self.devnull)
-        if (not result.returncode):
-            self.reachable_hosts.append(ip_address)
-            
-        with self.counter_lock:
-            self.completed_count += 1
+        try:
+            run(f'ping -c 1 -W 0.51 {ip_address}',
+                shell=True, check=True, stdout=DEVNULL, stderr=DEVNULL)
+        except SubprocessError:
+            pass
+        else:
+            self._reachable_hosts.append(ip_address)
+
+        with self._counter_lock:
+            self._completed_count += 1
             self._progress()
-        
+
     def _main(self):
         threads = []
-        for ip_address in self.ip_network:
-            icmp = threading.Thread(target=self._icmp, args=(ip_address,))
-            icmp.start()
-            threads.append(icmp)
+        for ip_address in self._ip_network:
+            threads.append(threading.Thread(target=self._icmp, args=(ip_address,)))
 
+        # starting all threads. 1 for each host.
+        for thread in threads:
+            thread.start()
+        # waiting for all threads to complete
         for thread in threads:
             thread.join()
 
-        self.sweep_complete = True
+        self._sweep_complete.set()
 
     def _complete(self):
         print('\n' + '-' * 26)
-        print(f'completed in {self.time_to_sweep} seconds')
-        if (self.reachable_hosts):
-            print('||Following IPs are UP||')
-            print('-' * 26)
-        else:
+        print(f'completed in {self._time_to_sweep} seconds')
+        print('-' * 26)
+        if (not self._reachable_hosts):
             print('NO HOSTS RESPONDED :(.')
+        else:
+            for ip in self._reachable_hosts:
+                print(str(ip))
 
-        for ip_address in sorted(self.reachable_hosts):
-            print(ip_address)
-            
     def _timer(self, start):
-        while True:
-            if (self.sweep_complete):
-                self.time_to_sweep = round(time.time() - start, 2)
-                break
-            
-            time.sleep(.001)
-            
+        self._sweep_complete.wait()
+
+        self._time_to_sweep = round(time.time() - start, 2)
+
     def _progress(self):
 	    bar_len = 38
-	    filled_len = int(round(bar_len * self.completed_count / float(self.ip_count)))
-	    percents = round(100.0 * self.completed_count / float(self.ip_count), 1)
+	    filled_len = int(round(bar_len * self._completed_count / float(self._ip_count)))
+	    percents = round(100.0 * self._completed_count / float(self._ip_count), 1)
 	    bar = '#' * filled_len + '=' * (bar_len - filled_len)
-	    sys.stdout.write(f'{self.completed_count}/{self.ip_count} || [{bar}] {percents}%s\r')
+	    sys.stdout.write(f'{self._completed_count}/{self._ip_count} || [{bar}] {percents}%s\r')
 	    sys.stdout.flush()
 
 def scan_again():
@@ -100,7 +96,8 @@ def scan_again():
         if (user_input in ['y', '']):
             break
         elif (user_input == 'n'):
-            exit('exiting...')
+            print('exiting...')
+            os._exit(0)
         else:
             print('invalid entry. try again.')
 
@@ -113,4 +110,6 @@ def main():
         scan_again()
 
 if __name__ == '__main__':
+    print('Ping sweep| by DOWRIGHT. ^_^')
+    print('-' * 26)
     main()
